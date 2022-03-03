e37ec0c51cfa8fb661280f99f6a3c7b87c2f2417
imy7 <34760601+imy7@users.noreply.github.com>
2022-03-03 21:55:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/85.8.15.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/imy7/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/imy7/g' package/base-files/files/bin/config_generate
+sed -i 's/ssid=OpenWrt/ssid=imy7/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
